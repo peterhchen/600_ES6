9bc0921e332a18ad9fa74bc0468be486f61a8f9d
peterhchen <peter.hp.chen@gmail.com>
2020-04-15 11:50:56 -0700

    var scope and hoisting

---
+++ 05-var-scope/05_var.ts
@@ -0,0 +1,12 @@
+function greetPerson (name) {
+    var greet
+    if (name === 'Peter') {
+        //var greet = "Hello Peter"
+        greet = "Hello Peter"
+    } else {
+        //var greet = "Hi there"
+        greet = "Hi there"
+    }
+    console.log (greet)
+}
+greetPerson ("Peter")
---
+++ 05-var-scope/scripts/05_var.js
@@ -0,0 +1,15 @@
+"use strict";
+function greetPerson(name) {
+    var greet;
+    if (name === 'Peter') {
+        //var greet = "Hello Peter"
+        greet = "Hello Peter";
+    }
+    else {
+        //var greet = "Hi there"
+        greet = "Hi there";
+    }
+    console.log(greet);
+}
+greetPerson("Peter");
+//# sourceMappingURL=05_var.js.map
